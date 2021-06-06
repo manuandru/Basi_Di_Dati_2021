4950c8cf4bfe3e2eb9ef1f259306b1652eb9402b
Manuel Andruccioli <manuandru987@gmail.com>
2021-06-06 21:57:38 +0200

    Aggiunte le prime DDL

---
+++ DDL/Insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO CLIENTI VALUES
+(1, '3331231231', NULL, NULL, NULL, '11111111111', 'Azienda 1'),
+(2, '3333213213', 'CF12345678912345', 'Mario', 'Rossi', NULL, NULL),
+(3, '3331112223', 'fc12345678912345', 'Manuel', 'Andruccioli', NULL, NULL),
+(4, '+3331112223', 'fc22222678912345', 'Manuel2', 'Andruccioli', NULL, NULL),
+(5, '-3331112223', 'fc11111678912345', 'Manuel3', 'Andruccioli', NULL, NULL);
+
+INSERT INTO IMPIANTI_ELETTRICI VALUES
+(1, '06/06/2021', '07/06/2021', 'Emilia-Romagna', 'Riccione', 'via', 123, 'Bella vista', 1),
+(2, '03/06/2020', null, 'Emilia-Romagna', 'Riccione', 'via', 123, 'Bella vista', 2),
+(3, '06/06/2021', '07/06/2021', 'Emilia-Romagna', 'Rimini', 'via', 123, 'Bella vista', 3),
+(4, '06/06/2021', '07/06/2021', 'Emilia-Romagna', 'Riccione', 'via', 123, 'Bella vista', 1),
+(5, '06/06/2021', '07/06/2021', 'Emilia-Romagna', 'Riccione', 'via', 123, 'Bella vista', 2);
+
+
---
+++ DDL/TecnoimpiantiDDL.sql
@@ -0,0 +1,35 @@
+/* DDL for Tecnoimpianti Database */
+
+CREATE TABLE CLIENTI
+(
+	CodCliente      INT             NOT NULL PRIMARY KEY, 
+    Telefono        VARCHAR(15)     NOT NULL, 
+    CodiceFiscale   NCHAR(16)       NULL /* UNIQUE */, 
+    Nome            VARCHAR(32)     NULL, 
+    Cognome         VARCHAR(32)     NULL, 
+    PartitaIVA      NCHAR(11)       NULL /* UNIQUE */, 
+    Denominazione   VARCHAR(50)     NULL, 
+    CONSTRAINT CodiceFiscaleXORPartitaIVA
+	CHECK 
+	(
+		(CodiceFiscale IS NOT NULL AND NOME IS NOT NULL AND COGNOME IS NOT NULL) 
+		OR
+		(PartitaIVA IS NOT NULL AND Denominazione IS NOT NULL)
+	)
+);
+
+
+CREATE TABLE IMPIANTI_ELETTRICI
+(
+	CodImpianto     INT             NOT NULL PRIMARY KEY, 
+    DataInizio      DATE            NOT NULL, 
+    DataFine        DATE            NULL, 
+    Regione         VARCHAR(32)     NOT NULL, 
+    Città           VARCHAR(32)     NOT NULL, 
+    Via             VARCHAR(32)     NOT NULL, 
+    Numero          INT             NOT NULL, 
+    Note            VARCHAR(256)    NULL,
+    CodCliente      INT             NOT NULL,
+    CONSTRAINT DataInizioMaggioreDataFine CHECK (DataInizio < DataFine),
+    CONSTRAINT FK_COD_CLIENTE FOREIGN KEY (CodCliente) REFERENCES CLIENTI(CodCliente)
+);
