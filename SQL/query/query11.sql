76b4d61b21a09164a4d93fcfe5cb170e95042659
Manuel Andruccioli <manuandru987@gmail.com>
2021-06-07 19:33:30 +0200

    minor fixes

--- SQL/query/query11.sql
+++ SQL/query/query11.sql
@@ -3,5 +3,6 @@ FROM ELETTRICISTI E, ELETTRICISTI_CON_RUOLI ER, TURNI_LAVORATIVI T
 WHERE E.CodiceFiscale = ER.CodiceFiscale
 AND ER.CodiceFiscale = T.CodiceFiscale
 AND ER.DataInizio = T.DataInizioElettricista
+AND DATEDIFF(DAY, T.DataLavoro, GETDATE()) < 30 
 AND ER.CodiceFiscale = <CodiceFiscale>
 GROUP BY ER.CodiceFiscale, E.Nome
