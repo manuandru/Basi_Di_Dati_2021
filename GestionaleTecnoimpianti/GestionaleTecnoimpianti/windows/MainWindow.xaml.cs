65e7011636356650c0514ac5403db290e6b5937a
Manuel Andruccioli <manuandru987@gmail.com>
2021-06-13 00:59:45 +0200

    First queries for clients

--- GestionaleTecnoimpianti/GestionaleTecnoimpianti/windows/MainWindow.xaml.cs
+++ GestionaleTecnoimpianti/GestionaleTecnoimpianti/windows/MainWindow.xaml.cs
@@ -24,5 +24,56 @@ namespace GestionaleTecnoimpianti
         {
             InitializeComponent();
         }
+
+        private ClassesTecnoimpiatiDBDataContext GetDB()
+            => new ClassesTecnoimpiatiDBDataContext();
+
+        private void Elenco_Clienti_Click(object sender, RoutedEventArgs e)
+        {
+            var data = from c in GetDB().CLIENTI
+                       select new
+                       {
+                           c.CodCliente,
+                           c.CodiceFiscale,
+                           c.PartitaIVA,
+                           c.Nome,
+                           c.Cognome,
+                           c.Denominazione,
+                           c.Telefono
+                       };
+
+            ClientiDataGrid.ItemsSource = data;
+        }
+
+        private void Elenco_Privati_Click(object sender, RoutedEventArgs e)
+        {
+            var data = from c in GetDB().CLIENTI
+                       where c.CodiceFiscale != null
+                       select new
+                       {
+                           c.CodCliente,
+                           c.CodiceFiscale,
+                           c.Nome,
+                           c.Cognome,
+                           c.Telefono
+                       };
+
+            ClientiDataGrid.ItemsSource = data;
+        }
+
+        private void Elenco_Aziende_Click(object sender, RoutedEventArgs e)
+        {
+            var data = from c in GetDB().CLIENTI
+                       where c.PartitaIVA != null
+                       select new
+                       {
+                           c.CodCliente,
+                           c.PartitaIVA,
+                           c.Denominazione,
+                           c.Telefono
+                       };
+
+            ClientiDataGrid.ItemsSource = data;
+        }
     }
 }
