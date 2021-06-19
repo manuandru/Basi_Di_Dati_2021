02f7ed0f19896bd4a80ef8b5d3936ea53562e28b
Manuel Andruccioli <manuandru987@gmail.com>
2021-06-19 17:54:41 +0200

    Finish form Turni Lavorativi

--- GestionaleTecnoimpianti/GestionaleTecnoimpianti/windows/turniLavorativi/FormTurniLavorativi.xaml.cs
+++ GestionaleTecnoimpianti/GestionaleTecnoimpianti/windows/turniLavorativi/FormTurniLavorativi.xaml.cs
@@ -19,9 +19,78 @@ namespace GestionaleTecnoimpianti.windows.turniLavorativi
     /// </summary>
     public partial class FormTurniLavorativi : Window
     {
+        private readonly ClassesTecnoimpiatiDBDataContext db = new ClassesTecnoimpiatiDBDataContext();
+
         public FormTurniLavorativi()
         {
             InitializeComponent();
+
+            var Elettricisti = from e in db.ELETTRICISTI_CON_RUOLI
+                               where e.DataFine == null
+                               select Tuple.Create(e.CodiceFiscale, e.DataInizio);
+
+            var Impianti = from i in db.IMPIANTI_ELETTRICI
+                           select i.CodImpianto;
+
+            var Furgoni = from i in db.FURGONI
+                          select i.Targa;
+
+            CodElettricista.ItemsSource = Elettricisti;
+            CodImpianto.ItemsSource = Impianti;
+            Targa.ItemsSource = Furgoni;
+        }
+
+        private void Annulla_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Inserisci_Click(object sender, RoutedEventArgs e)
+        {
+            if (CodElettricista.SelectedIndex != -1 && CodImpianto.SelectedIndex != -1
+                && DataLavoro.SelectedIndex != -1 && Targa.SelectedIndex != -1
+                && OraInizio.Text != "" && OraInizio.Text != "")
+            {
+                Tuple<string, DateTime> selectedCodElettricista = (Tuple<string, DateTime>)CodElettricista.SelectedItem;
+
+                TURNI_LAVORATIVI NewTurnoLavorativo = new TURNI_LAVORATIVI()
+                {
+                    CodiceFiscale = selectedCodElettricista.Item1,
+                    DataInizioElettricista = selectedCodElettricista.Item2,
+                    CodImpianto = (int)CodImpianto.SelectedItem,
+                    DataLavoro = (DateTime)DataLavoro.SelectedItem,
+                    Targa = (string)Targa.SelectedItem,
+                    OraInizio = TimeSpan.Parse(OraInizio.Text),
+                    OraFine = TimeSpan.Parse(OraFine.Text),
+                    Nota = Nota.Text,
+                };
+
+                db.TURNI_LAVORATIVI.InsertOnSubmit(NewTurnoLavorativo);
+
+                try
+                {
+                    db.SubmitChanges();
+                    MessageBox.Show("Inserimento avvenuto con successo");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Violazione Database: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Inserire tutti i campi correttamente");
+            }
+        }
+
+        private void CodImpianto_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CodImpianto.SelectedIndex != -1)
+            {
+                var Date = from l in db.LAVORI
+                           where l.CodImpianto == ((int)CodImpianto.SelectedItem)
+                           select l.Data;
+
+                DataLavoro.ItemsSource = Date;
+            }
         }
     }
 }
